9ebea5cba760d78f462f99d16c007dcc1da9b227
Sunflower <ziwenxu@stu.pku.edu.cn>
2026-04-09 22:23:01 +0800

    add file

--- assignment2/cs231n/setup.py
+++ assignment2/cs231n/setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+from pathlib import Path
 import numpy
+
+HERE = Path(__file__).resolve().parent
 
 extensions = [
     Extension(
-        "im2col_cython", ["im2col_cython.pyx"], include_dirs=[numpy.get_include()]
+        "im2col_cython",
+        [str(HERE / "im2col_cython.pyx")],
+        include_dirs=[numpy.get_include()],
     ),
 ]
 
